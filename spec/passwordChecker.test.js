be780e183802dd7082f3e863e54ad94a8db92576
Paoli <sacha.paoli@student.vinci.be>
2025-01-17 16:53:18 +0100

    add : implement special character validation for passwords and update tests

--- spec/passwordChecker.test.js
+++ spec/passwordChecker.test.js
@@ -14,4 +14,14 @@ describe("Password Checker", () => {
   it("should return true if the password has at least 8 characters", () => {
     expect(main.validPasswordLength("longenough")).toBe(true);
   });
+
+  it("should return false if the password does not contain a special character", () => {
+    expect(main.validPasswordSpecialChar("Password1")).toBe(false);
+  });
+
+  it("should return true if the password contains a special character", () => {
+    expect(main.validPasswordSpecialChar("Password1!")).toBe(true);
+  });
+
+  
 });
--- src/passwordChecker.js
+++ src/passwordChecker.js
@@ -5,6 +5,17 @@ class Main {
     }
     return true;
   }
+
+  validPasswordSpecialChar(password) {
+    if (password.match(/[^A-Za-z0-9]/)) {
+      return true;
+    }
+    return false;
+  }
+
+  
+
+
 }
 
 module.exports = Main;
