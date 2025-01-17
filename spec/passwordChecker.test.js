ab699fd57d535638620be422b55539812ffd67b3
Paoli <sacha.paoli@student.vinci.be>
2025-01-17 16:48:34 +0100

    add : implement password length validation and update tests

--- spec/passwordChecker.test.js
+++ spec/passwordChecker.test.js
@@ -1,5 +1,17 @@
+const Main = require('../src/passwordChecker');
+
 describe("Password Checker", () => {
-    it("should return true for a valid password", () => {
-      expect(true).toBe(true);
-    });
+  let main;
+
+  beforeEach(() => {
+    main = new Main();
   });
+
+  it("should return false if the password is shorter than 8 characters", () => {
+    expect(main.validPasswordLength("short")).toBe(false);
+  });
+
+  it("should return true if the password has at least 8 characters", () => {
+    expect(main.validPasswordLength("longenough")).toBe(true);
+  });
+});
--- src/passwordChecker.js
+++ src/passwordChecker.js
@@ -1,8 +1,11 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+class Main {
+  validPasswordLength(password) {
+    if (password.length < 8) {
+      return false;
+    }
+    return true;
+  }
+}
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+module.exports = Main;
 
